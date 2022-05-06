2a510b9ab6b62b45ec82f88c2fcad4ac7fd58a95
Ben <bmsteve@yahoo.com>
2022-05-06 17:10:59 +0300

    test getting pull request info

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
-async function run(): Promise<void> {
+// getting env variables
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+const octokit = github.getOctokit(GITHUB_TOKEN)
+const {context = {}}: any = github
+
+async function run(): Promise<any> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    console.log('context info', context?.payload)
+    // get pull request number
+    const pullNumber = context.payload?.head_commit?.message
+      ?.split(' ')
+      ?.find((o: any) => o?.includes('#'))
+      ?.split('#')[1]
+    console.log('pull number', pullNumber)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // fetch pull request
+    const pullRequest = await octokit.rest.pulls.get({
+      owner: context.payload?.repository?.owner?.login,
+      repo: context.payload?.repository?.name,
+      pull_number: pullNumber
+    })
+    console.log('pull request info', pullRequest?.data)
 
-    core.setOutput('time', new Date().toTimeString())
+    return pullRequest?.data
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
